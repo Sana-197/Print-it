64f438237b78480130c7fd8b007a698a667a64e4
Sana-197 <lilou-sb@hotmail.fr>
2024-03-05 18:30:46 +0100

    Adding arrows listenerEvents and dots to the slider

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,34 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+//Ajout des bulletPoints//
+
+ const dots = document.querySelector('.dots');
+ for (let i = 0; i < slides.length; i++) {
+	const dot = document.createElement("div");
+	dot.setAttribute( "class","dot");
+	dots.appendChild(dot);
+
+	if (i === 0) {
+        dot.classList.add("dot_selected");
+    }
+ }
+
+
+
+// Ajout d'EventListener sur la flèche gauche //
+
+ const flecheGauche = document.querySelector(".arrow_left");
+ flecheGauche.addEventListener('click', function(){
+
+	console.log("Vous avez cliqué sur la flèche gauche !");
+
+ });
+// Ajout d'EventListener sur la flèche droite//
+
+ const flecheDroite = document.querySelector(".arrow_right");
+ flecheDroite.addEventListener('click', function(){
+
+	console.log("Vous avez cliqué sur la flèche droite !");
+
+ });
